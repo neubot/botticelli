68b9243c5d66de9679a9c34a3953e8df1f0301da
Simone Basso <bassosimone@gmail.com>
2016-05-05 22:02:19 +0200

    factor out random code

--- dash.go
+++ dash.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"math/rand"
 	"net/http"
 	"strconv"
 	"strings"
@@ -50,36 +49,6 @@ func DashDownload(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "video/mp4")
 	w.Header().Set("Content-Length", resource_size)
 	w.Write(RandByte(body_size))
-}
-
-// XXX taken from https://stackoverflow.com/questions/22892120\
-// /how-to-generate-a-random-string-of-a-fixed-length-in-golang
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
-func RandByte(n int) []byte {
-	b := make([]byte, n)
-	src := rand.NewSource(123455)
-
-	// A src.Int63() generates 63 random bits, enough for
-	// letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
-	}
-
-	return b
 }
 
 func DashNegotiate(w http.ResponseWriter, r *http.Request) {
---
+++ random.go
@@ -0,0 +1,35 @@
+// Taken from https://stackoverflow.com/questions/22892120
+
+package main
+
+import (
+	"math/rand"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandByte(n int) []byte {
+	b := make([]byte, n)
+	src := rand.NewSource(123455)
+
+	// A src.Int63() generates 63 random bits, enough for
+	// letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return b
+}
