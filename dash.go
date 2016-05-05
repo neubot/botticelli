557cd80a3a1fac3631b4546e131bd5d645978e49
Simone Basso <bassosimone@gmail.com>
2016-05-05 21:59:20 +0200

    match original neubot implementation

--- dash.go
+++ dash.go
@@ -4,47 +4,50 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"regexp"
 	"strconv"
 	"strings"
 )
 
+const default_body_size = 1000
+const maximum_body_size = 104857600
+const maximum_repetitions = 60
+
 func DashDownload(w http.ResponseWriter, r *http.Request) {
+	var err error
 
-	//DASH_DEFAULT_BODY_SIZE := 1000
-	DASH_MAXIMUM_BODY_SIZE := 104857600
-	DASH_MAXIMUM_REPETITIONS := 60
-
-	if !strings.HasPrefix(r.URL.Path, "/dash/download/") {
+	// For robustness
+	if !strings.HasPrefix(r.URL.Path, "/dash/download") {
 		log.Println("dash: unexpected URI")
 		http.NotFound(w, r)
+		return
 	}
 
 	// TODO: limit maximum number of requests from a client
 
-	//body_size := DASH_MAXIMUM_BODY_SIZE
-	pattern := regexp.MustCompile(`/dash/download/([0-9+]*)$`)
-	resource_size := pattern.FindStringSubmatch(r.URL.Path)
-	if len(resource_size) < 2 {
-		log.Println("Body size not valid")
-		return
+	body_size := default_body_size
+	resource_size := strings.Replace(r.URL.Path, "/dash/download", "", -1)
+	if strings.HasPrefix(resource_size, "/") {
+		resource_size = resource_size[1:]
 	}
-
-	body_size, err := strconv.Atoi(resource_size[1])
-	if err != nil {
-		log.Println("dash: error body_syze type cast")
-		return
+	if resource_size != "" {
+		body_size, err = strconv.Atoi(resource_size)
+		if err != nil {
+			log.Println("dash: error body_syze type cast")
+			http.NotFound(w, r)
+			return
+		}
 	}
 
 	if body_size < 0 {
 		log.Println("dash: negative body size")
+		http.NotFound(w, r)
 		return
 	}
-	if body_size > DASH_MAXIMUM_BODY_SIZE {
-		body_size = DASH_MAXIMUM_BODY_SIZE
+	if body_size > maximum_body_size {
+		body_size = maximum_body_size
 	}
 
-	w.Header().Set("mimetype", "video/mp4")
+	w.Header().Set("Content-Type", "video/mp4")
 	w.Header().Set("Content-Length", resource_size)
 	w.Write(RandByte(body_size))
 }
