220dc1e34331405b58b56304178f975b65fffc81
DavideAllavena <davide.allavena@gmail.com>
2016-05-05 20:21:23 +0200

    DashDownload first implementation.

--- dash.go
+++ dash.go
@@ -1,8 +1,101 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"math/rand"
+	"regexp"
+	"strings"
+	"strconv"
 )
 
 func DashDownload(w http.ResponseWriter, r *http.Request) {
+
+	//DASH_DEFAULT_BODY_SIZE := 1000
+	DASH_MAXIMUM_BODY_SIZE := 104857600
+	DASH_MAXIMUM_REPETITIONS := 60
+
+	if strings.HasPrefix(r.URL.Path,"/dash/dowload/") == true {
+
+		count := 0
+		count++
+
+		if count > DASH_MAXIMUM_REPETITIONS {
+			log.Print("dash: too many repetitions")
+			return
+			//TODO close connection
+		}
+
+		//body_size := DASH_MAXIMUM_BODY_SIZE
+		pattern := regexp.MustCompile(`/dash/download/([0-9+]*)$`)
+		resource_size := pattern.FindStringSubmatch(r.URL.Path)
+		if len(resource_size) < 2 {
+			log.Println("Body size not valid")
+			return
+		}
+
+		body_size, err := strconv.Atoi(resource_size[1]);
+                if  err != nil {
+			log.Println("dash: error body_syze type cast")
+                        return
+		}
+
+		if body_size < 0 {
+			log.Println("dash: negative body size")
+			return
+		}
+		if body_size > DASH_MAXIMUM_BODY_SIZE {
+			body_size = DASH_MAXIMUM_BODY_SIZE
+		}
+
+		//XXX add corect pattern
+		text_pattern := "deadbeef"
+		body := RandString(body_size / len(text_pattern) + 1)
+
+		if len(body) > body_size {
+			body = body[:body_size]
+		}
+
+		w.Header().Set("code", "200")
+		w.Header().Set("reason", "Ok")
+		w.Header().Set("mimetype", "video/mp4")
+		fmt.Fprintf(w, body, r.URL.Path[1:])
+	} else {
+		log.Println("dash: unexpected URI")
+		return
+	}
+}
+
+
+// XXX taken from https://stackoverflow.com/questions/22892120\
+// /how-to-generate-a-random-string-of-a-fixed-length-in-golang
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+    letterIdxBits = 6                    // 6 bits to represent a letter index
+    letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+    letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+
+
+func RandString(n int) string {
+    b := make([]byte, n)
+    src := rand.NewSource(123455)
+
+    // A src.Int63() generates 63 random bits, enough for 
+    // letterIdxMax characters!
+    for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+        if remain == 0 {
+            cache, remain = src.Int63(), letterIdxMax
+        }
+        if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+            b[i] = letterBytes[idx]
+            i--
+        }
+        cache >>= letterIdxBits
+        remain--
+    }
+
+    return string(b)
 }
