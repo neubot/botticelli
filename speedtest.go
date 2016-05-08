87f3c68cc6970c72b5612ab3307cf66f14dc1638
DavideAllavena <davide.allavena@gmail.com>
2016-05-09 01:59:04 +0200

    First implementation of speedtest.
    Compiling but not working. We need some more work on this.

--- speedtest.go
+++ speedtest.go
@@ -2,12 +2,44 @@ package main
 
 import (
 	"net/http"
+	"regexp"
+	"strconv"
+	"time"
 )
+
+const TARGET = 5
+const CHUNK = 8192
 
 func SpeedtestCollect(w http.ResponseWriter, r *http.Request) {
 }
 
 func SpeedtestDownload(w http.ResponseWriter, r *http.Request) {
+
+	if r.Header.Get("range") != "" {
+		w.Header().Set("Content-Type", "application/octet-stream")
+
+		start_time := time.Now()
+		for time.Since(start_time).Seconds() >= TARGET {
+			w.Write(RandByte(CHUNK))
+		}
+		return
+	}
+
+	re := regexp.MustCompile("[0-9]+")
+	ranges := re.FindAllString(r.Header.Get("range"), -1)
+	ranges_int := []int{}
+
+	for _, i := range ranges {
+		j, err := strconv.Atoi(i)
+		if err != nil {
+			panic(err)
+		}
+		ranges_int = append(ranges_int, j)
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(RandByte(ranges_int[1] - ranges_int[0] - 1))
+
 }
 
 func SpeedtestNegotiate(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +47,10 @@ func SpeedtestNegotiate(w http.ResponseWriter, r *http.Request) {
 }
 
 func SpeedtestLatency(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(""))
+
 }
 
 func SpeedtestUpload(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(""))
 }
