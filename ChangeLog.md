1fc0d4bd5a1eccee2d46f78262b961d4648d9499
Simone Basso <bassosimone@gmail.com>
2016-08-07 14:47:31 +0200

    Release botticelli v0.0.1

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,2 +1,3 @@
+# botticelli 0.0.1 [2016-08-07]
 
-
+- Initial release with support for multistream NDT server
--- version.go
+++ version.go
@@ -1,4 +1,4 @@
 package main
 
-const Version = "0.0.1-dev"
+const Version = "0.0.1"
 const Product = "botticelli/" + Version
