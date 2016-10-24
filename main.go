83e461ccec2e81ed5ec6196f53a774c071cd3c0b
Simone Basso <bassosimone@gmail.com>
2016-10-24 10:32:18 +0200

    main.go: redirect logs on syslog

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/bassosimone/botticelli/common"
 	"github.com/bassosimone/botticelli/common/negotiate"
 	//"github.com/bassosimone/botticelli/nettests/bittorrent"
 	"github.com/bassosimone/botticelli/nettests/dash"
@@ -8,12 +9,25 @@ import (
 	//"github.com/bassosimone/botticelli/nettests/raw"
 	"github.com/bassosimone/botticelli/nettests/speedtest"
 	"log"
+	"log/syslog"
 	"net/http"
 	"math/rand"
 	"time"
 )
 
 func main() {
+	log.SetFlags(0)
+
+	// See http://technosophos.com/2013/09/14/using-gos-built-logger-log-syslog.html
+	log.Print("redirecting logs to the system logger")
+	logwriter, err := syslog.New(syslog.LOG_NOTICE, "botticelli")
+	if err != nil {
+		log.Fatal("cannot initialize syslog")
+	}
+
+	log.SetOutput(logwriter)
+	log.Printf("botticelli neubot server %s starting up", common.Version)
+
 	// Make sure we seed the random number generator properly
 	//   see <http://stackoverflow.com/a/12321192>
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -35,7 +49,7 @@ func main() {
 	http.HandleFunc("/", http.NotFound)
 
 	server := &http.Server{Addr: ":8080", Handler: nil}
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
